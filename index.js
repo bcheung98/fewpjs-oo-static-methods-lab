6e30f37bae916f8ecbc8b4931c6893cbb95c0c86
Brian <briantcheung@gmail.com>
2021-03-09 11:33:58 -0800

    commit tree cb8086487c3e0c5cd15e5815e3b5e7ef999f5af0

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    let newStr = [];
+    let strArr = str.split("");
+    newStr.push(strArr[0].toUpperCase());
+    for (let i = 1; i < strArr.length; i++) {
+      newStr.push(strArr[i]);
+    }
+    return newStr.join("");
+  }
+
+  static sanitize(str) {
+    let allowedChars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-' ";
+    let newStr = [];
+    let strArr = str.split("");
+    for (let i = 0; i < strArr.length; i++) {
+      if (allowedChars.includes(strArr[i])) {
+        newStr.push(strArr[i]);
+      }
+    }
+    return newStr.join("");
+  }
+
+  static titleize(str) {
+    let specialWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let newStr = [];
+    let words = str.split(" ");
+    newStr.push(this.capitalize(words[0]));
+    for (let i = 1; i < words.length; i++) {
+      if (!specialWords.includes(words[i])) {
+        newStr.push(this.capitalize(words[i]))
+      }
+      else {
+        newStr.push(words[i]);
+      }
+    }
+    return newStr.join(" ");
+  }
 }
